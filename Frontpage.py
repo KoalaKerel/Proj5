576796110eeac044381ab2cd145aa0e88ee09ab8
Damon B <damonbrock@live.nl>
2022-12-23 19:10:47 +0100

    Add files via upload

--- Frontpage.py
+++ Frontpage.py
@@ -29,6 +29,7 @@ if 'wrongdata' not in sl.session_state:
     sl.session_state['wrongdata']= False
 if 'datainput' not in sl.session_state:
     sl.session_state['datainput'] = pd.read_excel('Empty Schedule.xlsx')
+    sl.session_state['dienstdata2'] = pd.ExcelFile("Connexxion data - 2022-2023.xlsx")
 #Het uploaden van een planning
 sl.write("Please upload the excel file containing the bus schedule:")
 planning = sl.file_uploader('Schedule uploader', type=['xlsx'])
@@ -59,19 +60,35 @@ dienstregeling = pd.read_excel("Connexxion data - 2022-2023.xlsx")
 if 'dienstregeling' not in sl.session_state:
     sl.session_state['dienstdata'] =  dienstdata
     sl.session_state['dienstregeling'] = dienstregeling
+    sl.session_state['dienstdata2'] = pd.ExcelFile("Connexxion data - 2022-2023.xlsx")
 
 
     
 #Hier kan de state of health door de gebruiker bepaald worden. Uit zichzelf staat er altijd al 90%
 sl.write("Please input a percentage for the state of health:")
-soc = sl.text_input('State of Health', '90')
+soc = sl.number_input('State of Health')
 
-sl.write('The current state of health is ', soc, "%")
+
+if 'soc' not in sl.session_state or soc == 0:
+    sl.session_state['soc'] = 90
+    soc = 90
+elif int(soc) > 0:
+    sl.session_state['soc'] = float(soc)
+    
+sl.write('The current state of health is ', str(sl.session_state['soc']), " %")
 if float(soc) < 85:
     sl.markdown("Beware! This is a very low value. Recommended: 85% - 95%.")
 if float(soc) > 95:
     sl.markdown("Beware! This is a very high value. Recommended:  85% - 95%.")
-sl.session_state['soc'] = float(soc)
+
+sl.write("Please enter how many kwh the bus uses per meter:")
+verbruik = sl.number_input('Usage', format='%.4f')
+if 'stroomverbruik' not in sl.session_state:
+    sl.session_state['stroomverbruik'] = 0.00100
+elif verbruik > 0:
+    sl.session_state['stroomverbruik']=verbruik
+sl.markdown("The current usage is " + str(sl.session_state['stroomverbruik']) + ' kwh per meter.')
+    
 
 if planning is not None and mismatch==False: 
     #Welke verschillende lijnen zitten er in de omloopplanning
@@ -116,9 +133,10 @@ if planning is not None and mismatch==False:
             data['end_time'][i] = data['end_time'][i] + 1440
         if data['buslijn'][i] not in lijnen:
             data['buslijn'][i] = ""
+    
+
 if (planning is not None) and (sl.session_state['mismatch'] == False):            
     if(all(x in sl.session_state['dienstdata'].activiteit.unique() for x in sl.session_state['datainput'].type.unique() ))==False:
         sl.session_state['wrongdata'] = True
-        sl.markdown("a")
     else:
         sl.session_state['wrongdata'] = False
